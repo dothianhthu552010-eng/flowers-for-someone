85ef17804b7540eab48edd2b6a3aa70ce012551f
dothianhthu552010-eng <dothianhthu552010@gmail.com>
2026-03-08 11:57:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Chúc nii yêu 8-3 vui vẻ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
